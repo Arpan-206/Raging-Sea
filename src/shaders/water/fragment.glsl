4310ec5a5a98f0332a033208f3ace1de3263e5ed
Arpan Pandey <arpan@hackersreboot.tech>
2023-03-31 18:25:28 +0530

    Hello

---
+++ src/shaders/water/fragment.glsl
@@ -0,0 +1,13 @@
+uniform vec3 uSurfaceColor;
+uniform vec3 uDepthColor;
+uniform float uColorOffset;
+uniform float uColorMultiplier;
+
+varying float vElevation;
+
+void main()
+{
+    float mixStrength = (vElevation + uColorOffset) * uColorMultiplier;
+    vec3 color = mix(uSurfaceColor, uDepthColor, mixStrength);
+    gl_FragColor = vec4(color, 1.0);
+}
